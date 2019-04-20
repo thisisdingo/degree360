b07d08ba9e8ddd1d255007049674ccd5f94aabb1
uraankhayayaal <uraankhayayaal@gmail.com>
2019-04-20 13:29:51 +0900

    register

---
+++ degree360/Controllers/Register/RegisterInteractor.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterInteractor.swift
+//  degree360
+//
+//  Created by Ayaal Kaplin on 20/04/2019.
+//  Copyright © 2019 pizza. All rights reserved.
+//
+
+import UIKit
+
+protocol RegisterInteractorProtocol : BaseInteractorProtocol {
+    func successRegister()
+}
+
+class RegisterInteractor : BaseInteractor {
+    
+    
+    init(_ delegate : BaseInteractorProtocol){
+        self.delegate = delegate
+    }
+    
+    func register(_ name: String, _ photo: UIImage){
+        
+    }
+}
---
+++ degree360/Controllers/Register/RegisterVC.swift
@@ -0,0 +1,38 @@
+//
+//  RegisterVC.swift
+//  degree360
+//
+//  Created by Ayaal Kaplin on 20/04/2019.
+//  Copyright © 2019 pizza. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC : UIViewController, RegisterInteractorProtocol {
+    func showMessage(_ message: String) {
+        
+    }
+    
+    func startLoading() {
+        
+    }
+    
+    func hideLoading() {
+        
+    }
+    
+    func successRegister() {
+        
+    }
+    
+    var interactor : RegisterInteractor!
+    
+    override func viewDidLoad() {
+        self.interactor = RegisterInteractor(self)
+        
+        // on click
+        // validate
+        self.interactor.register(<#T##name: String##String#>, <#T##photo: UIImage##UIImage#>)
+    }
+    
+}
