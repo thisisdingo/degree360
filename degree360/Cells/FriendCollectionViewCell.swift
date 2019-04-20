c13888d73745d8e9a0a3ebe848ef6ec67ba846b3
uraankhayayaal <uraankhayayaal@gmail.com>
2019-04-20 20:41:44 +0900

    collectionn view cell added

---
+++ degree360/Cells/FriendCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  FriendCollectionViewCell.swift
+//  degree360
+//
+//  Created by Ayaal Kaplin on 20/04/2019.
+//  Copyright © 2019 pizza. All rights reserved.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var friendPhoto: UIImageView!
+    @IBOutlet weak var friendName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
