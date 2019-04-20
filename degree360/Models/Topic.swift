e02d1273cc00ea7a71ce2a30d2887afca7ef907d
gamburger <mail@michil.ru>
2019-04-20 20:11:32 +0900

    Added topic model

--- degree360/Models/Topic.swift
+++ degree360/Models/Topic.swift
@@ -1,41 +1,31 @@
 //
-//  Lesson.swift
+//  File.swift
 //  degree360
 //
 //  Created by Mister Gamburger on 20/04/2019.
 //  Copyright © 2019 pizza. All rights reserved.
 //
 
-import Foundation
 import SwiftyJSON
 
-struct Lesson {
+struct Topic {
     var id : String
     var title : String
-    var description : String
-    var author : User
     var created : Date
     var updated : Date
-    var topics : [Topic]
     
     init() {
         self.id = ""
         self.title = ""
-        self.description = ""
-        self.author = User()
         self.created = Date()
         self.updated = Date()
-        self.topics = [Topic]()
     }
     
     init(_ json : JSON) {
         self.id = json["id"].stringValue
         self.title = json["title"].stringValue
-        self.description = json["description"].stringValue
-        self.author = User(json["author"])
         self.created = json["created"].stringValue.date
         self.updated = json["updated"].stringValue.date
-        self.topics = json["topics"].arrayValue.map({ return Topic($0) })
     }
     
 }
