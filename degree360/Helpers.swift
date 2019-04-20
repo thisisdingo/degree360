0e9b9b92cf8d9cc699504b68788b43ab750c197d
gamburger <mail@michil.ru>
2019-04-20 16:08:09 +0900

    Added helpers file

---
+++ degree360/Helpers.swift
@@ -0,0 +1,17 @@
+//
+//  Helpers.swift
+//  degree360
+//
+//  Created by Mister Gamburger on 20/04/2019.
+//  Copyright © 2019 pizza. All rights reserved.
+//
+
+import UIKit
+
+class Helpers {
+    
+    static func getNavController(_ rootVC : UIViewController) -> UIViewController {
+        return UINavigationController(rootViewController: rootVC)
+    }
+    
+}
