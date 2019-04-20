c4531a95d2566e9a8a0b36b451e01dff4f84fb1e
uraankhayayaal <uraankhayayaal@gmail.com>
2019-04-20 18:38:58 +0900

    ru to en

--- degree360/Extenstions/Extenstion+UIViewController.swift
+++ degree360/Extenstions/Extenstion+UIViewController.swift
@@ -12,7 +12,7 @@ extension UIViewController {
     
     func alert(_ text :  String) {
         let alert = UIAlertController(title: text, message: nil, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Закрыть", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Close", style: .cancel, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
     
