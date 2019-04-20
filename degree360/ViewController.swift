0d44fa75a2aa16f0c3d9c644f146ddee9ae937fc
gamburger <mail@michil.ru>
2019-04-20 12:55:40 +0900

    Initial Commit

---
+++ degree360/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  degree360
+//
+//  Created by Mister Gamburger on 20/04/2019.
+//  Copyright © 2019 pizza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
